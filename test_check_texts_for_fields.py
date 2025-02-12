df30f362cda76e84bc089ff7a10b835f63226896
NeHorgi <119290694+NeHorgi@users.noreply.github.com>
2025-02-12 17:27:50 +0100

    Adding fixture finalization.

--- test_check_texts_for_fields.py
+++ test_check_texts_for_fields.py
@@ -23,7 +23,9 @@ config = Config()
 
 @pytest.fixture(scope='session')
 def chrome_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
 
 
 def test_check_texts_for_required_fields(chrome_driver):
@@ -31,41 +33,36 @@ def test_check_texts_for_required_fields(chrome_driver):
     Test checks that all support texts for all required fields in the form of ordering Demo are correct.
     """
     chrome_driver.get(config.base_url)
+    demo_button = chrome_driver.find_element(By.XPATH, "//span[@class='w4Vxx6 wixui-button__label']")
+    demo_button.click()
+    time.sleep(2)
 
-    try:
-        demo_button = chrome_driver.find_element(By.XPATH, "//span[@class='w4Vxx6 wixui-button__label']")
-        demo_button.click()
-        time.sleep(2)
+    assert config.base_url in chrome_driver.current_url, "Form was not opened."
 
-        assert config.base_url in chrome_driver.current_url, "Form was not opened."
+    submit_button = chrome_driver.find_element(By.XPATH, "//button[@data-hook='submit-button']")
+    submit_button.click()
+    time.sleep(3)
 
-        submit_button = chrome_driver.find_element(By.XPATH, "//button[@data-hook='submit-button']")
-        submit_button.click()
-        time.sleep(3)
+    required_email_field_message = chrome_driver.find_element(By.XPATH, "//div[@data-hook='field-error-email']")
+    required_email_field_message_text = required_email_field_message.text
+    required_message_field_message = chrome_driver.find_element(By.XPATH,
+                                                                "//div[@data-hook='field-error-long_answer']")
+    required_message_field_message_text = required_message_field_message.text
+    required_check_box_field_message = chrome_driver.find_element(By.XPATH,
+                                                                  "//div[@data-hook='form-field-form_field_cea7']")
+    required_check_box_field_message_text = required_check_box_field_message.text.split('\n')[-1]
 
-        required_email_field_message = chrome_driver.find_element(By.XPATH, "//div[@data-hook='field-error-email']")
-        required_email_field_message_text = required_email_field_message.text
-        required_message_field_message = chrome_driver.find_element(By.XPATH,
-                                                                    "//div[@data-hook='field-error-long_answer']")
-        required_message_field_message_text = required_message_field_message.text
-        required_check_box_field_message = chrome_driver.find_element(By.XPATH,
-                                                                      "//div[@data-hook='form-field-form_field_cea7']")
-        required_check_box_field_message_text = required_check_box_field_message.text.split('\n')[-1]
+    assert (required_email_field_message_text ==
+            consts.email_field_expected_text), (f'Wrong text for the email field, expected: '
+                                                f'{consts.email_field_expected_text}, '
+                                                f'got: {required_email_field_message}')
 
-        assert (required_email_field_message_text ==
-                consts.email_field_expected_text), (f'Wrong text for the email field, expected: '
-                                                    f'{consts.email_field_expected_text}, '
-                                                    f'got: {required_email_field_message}')
+    assert (required_message_field_message_text ==
+            consts.message_field_expected_text), (f'Wrong text for the message field, expected: '
+                                                  f'{consts.message_field_expected_text}, '
+                                                  f'got: {required_message_field_message_text}')
 
-        assert (required_message_field_message_text ==
-                consts.message_field_expected_text), (f'Wrong text for the message field, expected: '
-                                                      f'{consts.message_field_expected_text}, '
-                                                      f'got: {required_message_field_message_text}')
-
-        assert (required_check_box_field_message_text ==
-                consts.check_box_expected_text), (f'Wrong text for the check box, expected: '
-                                                  f'{consts.check_box_expected_text}, '
-                                                  f'got: {required_check_box_field_message_text}')
-
-    finally:
-        chrome_driver.quit()
+    assert (required_check_box_field_message_text ==
+            consts.check_box_expected_text), (f'Wrong text for the check box, expected: '
+                                              f'{consts.check_box_expected_text}, '
+                                              f'got: {required_check_box_field_message_text}')
